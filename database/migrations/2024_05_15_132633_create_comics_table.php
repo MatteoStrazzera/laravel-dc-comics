da39506f8eec0729290a87b0df13ae8e4e052d8e
MatteoStrazzera <mattestrazz@gmail.com>
2024-05-15 16:24:05 +0200

    Add comics table and seed in database

--- database/migrations/2024_05_15_132633_create_comics_table.php
+++ database/migrations/2024_05_15_132633_create_comics_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description',)->nullable();
+            $table->string('thumb', 255);
+            $table->string('price', 15);
+            $table->string('series', 50);
+            $table->string('sale_date', 15);
+            $table->string('type', 20);
             $table->timestamps();
         });
     }
