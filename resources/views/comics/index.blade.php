1b6ccadc8721ba6bbf5bbb9b426128b2b7bd94fd
MatteoStrazzera <mattestrazz@gmail.com>
2024-05-15 17:25:45 +0200

    Add basic table in index to show comics info

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,4 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Thumb</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Sale date</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                @forelse ($comics as $comic)
+                    <tr class="">
+                        <td scope="row">{{ $comic->title }}</td>
+                        <td>{{ $comic->description }}</td>
+                        <td>
+                            <img width="60" src="{{ $comic->thumb }}" alt="">
+                        </td>
+                        <td>{{ $comic->price }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                        <td>{{ $comic->type }}</td>
+                        <td>View/Edit/Remove</td>
+                    </tr>
+                @empty
+                    <tr class="">
+                        <td scope="row"></td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                        <td>Not Found</td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 @endsection
